2fb406b8433cabc9b0a56bc5ff6f7471d0ae5560
Debangan Mandal <debangan.mandal@gmail.com>
2024-12-16 22:53:54 +0530

    custom vector env for PPO implement

---
+++ envs/quad_ppo_env.py
@@ -0,0 +1,142 @@
+import gymnasium as gym
+import numpy as np
+from gymnasium import utils
+from gymnasium.envs.mujoco import MujocoEnv
+from gymnasium.spaces import Box
+import os
+from typing import Dict, Tuple, Union
+
+DEFAULT_CAMERA_CONFIG = {
+    "distance": 4.0,
+}
+
+class QuadrupedWalkPPO(MujocoEnv, utils.EzPickle):
+    metadata = {"render_modes": {"human, rgb_array"}, "render_fps": 20}
+
+    def __init__(
+        self,
+        xml_file: str,
+        frame_skip: int = 5,
+        forward_reward_weight: float = 1,
+        ctrl_cost_weight: float = 0.05,
+        contact_cost_weight: float = 5e-4,
+        healthy_reward: float = 1.0,
+        main_body: Union[int, str] = 1,
+        terminate_when_unhealthy: bool = True,
+        healthy_z_range: Tuple[float, float] = (0.2, 1.0),
+        include_cfrc_ext_in_observation: bool = True,
+        exclude_current_positions_from_observation: bool = False,
+        reset_noise_scale: float = 0.1,
+        default_camera_config: Dict[str, Union[float, int]] = DEFAULT_CAMERA_CONFIG,
+        **kwargs,
+        ):
+
+        utils.EzPickle.__init__(
+            self,
+            xml_file,
+            frame_skip,
+            forward_reward_weight,
+            ctrl_cost_weight,
+            contact_cost_weight,
+            healthy_reward,
+            main_body,
+            terminate_when_unhealthy,
+            healthy_z_range,
+            include_cfrc_ext_in_observation,
+            exclude_current_positions_from_observation,
+            reset_noise_scale,
+            **kwargs
+            )
+        self._forward_reward_weight = forward_reward_weight
+        self._ctrl_cost_weight = ctrl_cost_weight
+        self._contact_cost_weight = contact_cost_weight
+
+        self._reset_noise_scale = reset_noise_scale
+
+        self._healthy_reward = healthy_reward
+        self._main_body = main_body
+        self._terminate_when_unhealthy = terminate_when_unhealthy
+        
+        self._healthy_z_range = healthy_z_range
+        self._exclude_current_positions_from_observation = (exclude_current_positions_from_observation)
+        self._include_cfrc_ext_in_observation = include_cfrc_ext_in_observation
+
+        self._main_body = main_body
+
+        super().__init__(
+            self,
+            os.path.normpath(os.path.join(os.path.dirname(__file__), "../go1_quadruped/go1.xml")),
+            frame_skip,
+            observation_space=None,
+            default_camera_config=default_camera_config,
+            **kwargs,
+        )
+
+        self.observation_space = Box(-np.inf, np.inf, (107,), dtype=np.float64)
+
+        self.observation_structure = {
+            "skipped_qpos": 2*exclude_current_positions_from_observation,
+            "qpos": self.data.qpos.size,
+            "qvel": self.data.qvel,
+            "cfrc_ext": self.data.cfrc_ext[1:].size*include_cfrc_ext_in_observation,
+        }
+
+    @property
+    def contact_cost(self):
+        contact_cost = self._contact_cost_weight * np.sum(np.square(self.contact_forces))
+        return contact_cost
+
+    def control_cost(self, action):
+        control_cost = self._ctrl_cost_weight * np.sum(np.square(action))
+        return control_cost
+
+    def step(self, action):
+        # Simulation
+        xy_position_before = self.data.body(self._main_body).xpos[:2].copy()
+        self.do_simulation(action, self.frame_skip)
+        xy_position_after = self.data.body(self._main_body).xpos[:2].copy()
+
+        # Update new x and y velocities
+        xy_velocity = (xy_position_after - xy_position_before)/self.dt
+        x_velocity, y_velocity = xy_velocity
+
+        # Get observation
+        observation = self._get_obs()
+        
+        # Get rewards
+        control_cost = self.control_cost(action)
+        contact_cost = self.contact_cost
+        reward = (x_velocity*self._forward_reward_weight+self._healthy_reward) - (control_cost+contact_cost)
+        
+        # Check termination
+        min_z, max_z = self._healthy_z_range
+        state = self.state_vector()
+        is_healthy = np.isfinite(state).all() and min_z <= state[2] <= max_z
+        terminated = (not is_healthy) and self._terminate_when_unhealthy
+
+        if self.render_mode == "human":
+            self.render()
+
+        return observation, reward, terminated, False, {}
+
+    def reset_model(self):
+        noise_low = -self._reset_noise_scale
+        noise_high = self._reset_noise_scale
+
+        qpos = self.init_qpos + self.np_random.uniform(low=noise_low, high=noise_high, size=self.model.nq)
+        qvel = (self.init_qvel + self._reset_noise_scale * self.np_random.standard_normal(self.model.nv))
+
+        self.set_state(qpos, qvel)
+
+        observation = self._get_obs()
+        return observation
+
+    def _get_obs(self):
+        return {
+            "x-position": self.data.qpos[0],
+            "y-position": self.data.qpos[1],
+            "distance_from_origin": np.linalg.norm(self.data.qpos[0:2], ord=2)
+        }
+       
+        
+        
--- scripts/play.py
+++ scripts/play.py
@@ -4,31 +4,30 @@ from tqdm import tqdm
 from stable_baselines3 import SAC
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.vec_env import SubprocVecEnv
 import torch
 
-# env = gym.make('Ant-v5', render_mode = "human")
+env = gym.make('Ant-v5', render_mode = "human")
 
 # print(env.action_space)
 
-env = gym.make(
-    'Ant-v5',
-    xml_file='quadruped_simulation/go1_quadruped/go1.xml',
-    forward_reward_weight=1,
-    ctrl_cost_weight=0.05,
-    contact_cost_weight=5e-4,
-    healthy_reward=1,
-    main_body=1,
-    healthy_z_range=(0.195, 0.75),
-    include_cfrc_ext_in_observation=True,
-    exclude_current_positions_from_observation=False,
-    reset_noise_scale=0.1,
-    frame_skip=25,
-    max_episode_steps=1000,
-    render_mode = "human"
-)
+# env = gym.make(
+#     'Ant-v5',
+#     xml_file='~/Desktop/Addverb/examples/quadruped_simulation/go1_quadruped/go1.xml',
+#     forward_reward_weight=1,
+#     ctrl_cost_weight=0.05,
+#     contact_cost_weight=5e-4,
+#     healthy_reward=1,
+#     main_body=1,
+#     healthy_z_range=(0.195, 0.75),
+#     include_cfrc_ext_in_observation=True,
+#     exclude_current_positions_from_observation=False,
+#     reset_noise_scale=0.1,
+#     frame_skip=25,
+#     max_episode_steps=1000,
+#     render_mode = "human"
+# )
 
-model = SAC("MlpPolicy", env, verbose=1)
+"""model = SAC("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=100_000)
 # model.save("sac_ant")
 # model = SAC.load("sac_ant")
@@ -39,21 +38,38 @@ while True:
     action, _states = model.predict(obs, deterministic=True)
     obs, reward, terminated, truncated, info = env.step(action)
     if terminated or truncated:
-        obs, info = env.reset()
+        obs, info = env.reset()"""
 
-"""vec_env = make_vec_env("Ant-v5", n_envs=4)
-print(vec_env)
+# gym_vec_env = gym.make_vec("Ant-v5", num_envs=2, vectorization_mode="async")
+sb3_vec_env = make_vec_env("Ant-v5", n_envs=2)
+# print(gym_vec_env)
+# print(sb3_vec_env)
 
-model = PPO("MlpPolicy", vec_env, verbose=1)
+# Train and save the model
+# Saving the model is divided into 2 parts: one for RL parameters(data), and another for NN weights(parameters), acc. to the documentation
+
+model = PPO("MlpPolicy", sb3_vec_env, verbose=1)
 model.learn(total_timesteps=25000)
+
+# Save model
 # model.save("ppo_ant")
-# model = PPO.load("ppo_ant")
-obs, info = vec_env.reset()
+
+# Load the saved model
+# model = PPO.load("ppo_ant_copy.zip", env=sb3_vec_env)
+
+obs = sb3_vec_env.reset()
+action, _states = model.predict(obs)
+
+# print(model.action_space)
+# print(action)
+
+
+# print(obs)
+# print(sb3_vec_env.reset_infos)
 done = False
 
 while True:
     action, _states = model.predict(obs)
-    obs, rewards, terminated, truncated, info = vec_env.step(action)
-    vec_env.render("human") """
-
-
+    obs, rewards, done, info = sb3_vec_env.step(action)
+    sb3_vec_env.render("")
+sb3_vec_env.close()
