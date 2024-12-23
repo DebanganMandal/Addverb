da4f6addd508406a2689b60cfe896fdf4d08e499
Debangan Mandal <debangan.mandal@gmail.com>
2024-12-23 13:27:03 +0530

    source code altered (nm)

--- scripts/rough.py
+++ scripts/rough.py
@@ -82,11 +82,11 @@ from stable_baselines3.common.env_util import make_vec_env
 import cv2
 import imageio
 
-vec_env = QuadrupedWalkPPO(render_mode="human")
+vec_env = QuadrupedWalkPPO(render_mode="rgb_array")
 check_env(vec_env)
 ppo_vec_env = make_vec_env(QuadrupedWalkPPO, n_envs=1)
 model = PPO("MlpPolicy", ppo_vec_env, verbose=1, device="cpu")
-model.learn(total_timesteps=25000)
+model.learn(total_timesteps=2**20)
 # model.save("ppo_quadruped")
 
 # vec_env = QuadrupedWalkPPO(render_mode="rgb_array")
@@ -98,14 +98,13 @@ frames = []
 for _ in range(1000):
     action, _stages = model.predict(obs)
     obs, reward, done, truncated, info = vec_env.step(action)
-    # image = vec_env.render()
-    vec_env.render()
-    # if _%7 == 0:
-    #     frames.append(image)
+    image = vec_env.render()
+    if _%7 == 0:
+        frames.append(image)
 
     if done or truncated:
         obs, info = vec_env.reset()
 
-# with imageio.get_writer("../media/test.gif", mode="I") as writer:
-#     for idx, frame in enumerate(frames):
-#         writer.append_data(frame)
+with imageio.get_writer("../media/test.gif", mode="I") as writer:
+    for idx, frame in enumerate(frames):
+        writer.append_data(frame)
